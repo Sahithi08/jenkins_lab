f8b73176109cfcfbb4c3aeb7b742f77f28ff0d6e
Sahithi08 <79003985+Sahithi08@users.noreply.github.com>
2022-11-25 11:43:54 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -19,21 +19,21 @@ class Test_even(unittest.TestCase):
 #         """
 #         Test case to check if the number is even.
 #         """
-        x=2
-        result = even(x)
-        self.assertEqual(result,1)
-        x=3
-        result = even(x)
-        self.assertEqual(result,0)
-        x=4
-        result = even(x)
-        self.assertEqual(result,1)
-        x=15
-        result = even(x)
-        self.assertEqual(result,1)
-        x=24
-        result = even(x)
-        self.assertEqual(result,1)
+                x=2
+                result = even(x)
+                self.assertEqual(result,1)
+                x=3
+                result = even(x)
+                self.assertEqual(result,0)
+                x=4
+                result = even(x)
+                self.assertEqual(result,1)
+                x=15
+                result = even(x)
+                self.assertEqual(result,1)
+                x=24
+                result = even(x)
+                self.assertEqual(result,1)
 
 if __name__ == '__main__':
     unittest.main()
