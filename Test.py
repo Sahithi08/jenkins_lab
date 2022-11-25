b788ea0e768490b1d76185e5e5c622fccd8a6d32
Sahithi08 <79003985+Sahithi08@users.noreply.github.com>
2022-11-25 11:45:35 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -22,18 +22,18 @@ class Test_even(unittest.TestCase):
                 x=2
                 result = even(x)
                 self.assertEqual(result,1)
-                x=3
-                result = even(x)
-                self.assertEqual(result,0)
-                x=4
-                result = even(x)
-                self.assertEqual(result,1)
-                x=15
-                result = even(x)
-                self.assertEqual(result,1)
-                x=24
-                result = even(x)
-                self.assertEqual(result,1)
+#                 x=3
+#                 result = even(x)
+#                 self.assertEqual(result,0)
+#                 x=4
+#                 result = even(x)
+#                 self.assertEqual(result,1)
+#                 x=15
+#                 result = even(x)
+#                 self.assertEqual(result,1)
+#                 x=24
+#                 result = even(x)
+#                 self.assertEqual(result,1)
 
 if __name__ == '__main__':
     unittest.main()
