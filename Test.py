58a6c6283586f6b53ff951379f76a34525717362
Sahithi08 <79003985+Sahithi08@users.noreply.github.com>
2022-11-25 11:33:48 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
-# Test case for adding two numbers
 import unittest
 
-from Prog1 import add
+# from Prog1 import add
 from prog2 import even
 
 class TestSum(unittest.TestCase):
-    def test_add(self):
-        """
-        Test case to add two numbers
-        """
-        x=5
-        y=10
-        result = add(x,y)
-        self.assertEqual(result, 15)
+#     def test_add(self):
+#         """
+#         Test case to add two numbers
+#         """
+#         x=5
+#         y=10
+#         result = add(x,y)
+#         self.assertEqual(result, 15)
 
     def test_even(self):
         """
-        Test case to add two numbers
+        Test case to check if the number is even.
         """
-        x=10
+        x=2
         result = even(x)
-        self.assertEqual(result,5)
+        self.assertEqual(result,1)
+        x=3
+        result = even(x)
+        self.assertEqual(result,0)
+        x=4
+        result = even(x)
+        self.assertEqual(result,1)
+        x=15
+        result = even(x)
+        self.assertEqual(result,1)
+        x=24
+        result = even(x)
+        self.assertEqual(result,1)
 
 if __name__ == '__main__':
     unittest.main()
